48567705928cb2829e6bdbd237ce3028169fb758
javierfm <jadosfer@hotmail.com>
2021-06-17 11:02:05 -0300

    header, button and footer

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  text = "Default text"
 
   constructor() {}
+
+  onChageText() {
+    this.text = "Changed!"
+  }
 
 }
